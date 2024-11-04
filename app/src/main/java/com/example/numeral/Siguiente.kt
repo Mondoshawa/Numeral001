fbf24579836ca1aa537b4f0b4d667526ceb0bfa0
LuisFer <ingenieroluisfer@hotmail.com>
2024-11-04 17:25:18 -0500

    Subiendo taller

--- app/src/main/java/com/example/numeral/Siguiente.kt
+++ app/src/main/java/com/example/numeral/Siguiente.kt
@@ -1,31 +1,16 @@
 package com.example.numeral
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Siguiente : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-//Aquí debajo creamos la variable del botón para conectar actividades
-        val button = findViewById<Button>(R.id.button)
-
-
-        button.setOnClickListener {
-
-
-            val intent = Intent(this, Siguiente::class.java)
-            startActivity(intent)
-        }
-//Hasta aquí llega el botón de enlace a otra actividad
-
+        setContentView(R.layout.activity_siguiente)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
